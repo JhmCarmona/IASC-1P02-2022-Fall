e628edf11c9b364238609a0b047862b54ea5bd89
Jhon HM Carmona <jm18bk@brocku.ca>
2022-11-13 18:56:13 -0500

    Update script.js

---
+++ modules-w3-onwards/w9/script.js
@@ -0,0 +1,8 @@
+
+//To use any of the following 3 pop-ups, you must delete the "//" before them.
+
+//alert ("Alert!");
+
+//confirm ("Do you wanna enter the page?");
+
+//prompt ("What is 7 + 7?");
